3ce12befd2ae479d4eca0e1046461c195c001155
Tácio <tcgtavares@gmail.com>
2014-07-14 22:59:53 -0300

    creating method to delete created files before each test

--- test/test_wiki_breaker.py
+++ test/test_wiki_breaker.py
@@ -3,10 +3,28 @@ __author__ = 'tacio'
 
 import random
 import unittest
+import os
 
 
 class TestWikiBreaker(unittest.TestCase):
 
+    @staticmethod
+    def _clean_created_test_files():
+        print 'Cleaning created files...'
+        file_name = __file__.split(os.path.sep)[-1]
+        file_dir = __file__.replace(file_name, '')
+        created_files_dir = os.path.join(file_dir, 'files', 'created', 'articles-files')
+        for f in os.listdir(created_files_dir):
+            f = os.path.join(created_files_dir, f)
+            print 'Removing file: ' + f
+            os.remove(f)
+
+    @classmethod
+    def setUpClass(cls):
+        cls._clean_created_test_files()
+
+    def setUp(self):
+        self.seq = range(10)
 
     def test_shuffle(self):
         # make sure the shuffled sequence does not lose any elements
@@ -27,5 +45,5 @@ class TestWikiBreaker(unittest.TestCase):
         for element in random.sample(self.seq, 5):
             self.assertTrue(element in self.seq)
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
